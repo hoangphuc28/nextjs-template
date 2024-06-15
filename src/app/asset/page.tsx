088180c885dfe66c4c8ab6d48185aeae06defd7d
hoangphuc28 <zhoangp@gmail.com>
2024-06-15 16:32:01 +0700

    feat: initialize project

--- src/app/asset/page.tsx
+++ src/app/asset/page.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 export default function Index() {
   return (
     <Fragment>
+      Asset
     </Fragment>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'nashtech':'#cf2338',
+        'bluegray': '#eff1f5',
+        'cyangray': '#fafcfc'
+      },
     },
   },
   plugins: [],
